02503d22eeb03a6e0014c05cf6eaca9a2ec9d379
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-04-04 12:00:17 -0600

    Add animation to button press

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -1,53 +1,120 @@
-import React, { useState } from 'react';
-import { Pressable, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  Animated,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  View,
+} from 'react-native';
 
 const CustomButton = () => {
   const [text, setText] = useState('Press me');
   const [disabled, setDisabled] = useState(false);
-  let timeoutId = null;
+  const [pressed, setPressed] = useState(false);
+  const timeoutId1 = useRef(null);
+  const timeoutId2 = useRef(null);
+  const timeoutId3 = useRef(null);
+  const timeoutId4 = useRef(null);
+  const sizeAnim = useRef(new Animated.Value(1)).current;
+  const colorAnim = useRef(new Animated.Value(0)).current;
+  const backgroundColor = colorAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['rgb(115, 25, 99)', 'rgb(41, 112, 69)'],
+  });
 
   const handlePressIn = () => {
+    setPressed(true);
     setText('Are you sure?');
-    timeoutId = setTimeout(() => setText('Are you actually sure?'), 2000);
-    timeoutId = setTimeout(() => setText('Ok fine...'), 4000);
-    timeoutId = setTimeout(() => {
+    timeoutId1.current = setTimeout(() => setText('Are you actually sure?'), 2000);
+    timeoutId2.current = setTimeout(() => setText('Ok fine...'), 4000);
+    timeoutId3.current = setTimeout(() => {
       setDisabled(true);
       setText('Notifying Cameron...');
     }, 5000);
-    timeoutId = setTimeout(() => {
-      setText('He\'s been notified');
+    timeoutId4.current = setTimeout(() => {
+      setText("He's been notified!");
       setDisabled(false);
     }, 8000);
-    timeoutId = setTimeout(() => setText('Press me'), 9500);
-
+    const diagonal = Math.sqrt(
+      Math.pow(Dimensions.get('window').width, 2) +
+        Math.pow(Dimensions.get('window').height, 2)
+    );
+    Animated.parallel([
+      Animated.timing(sizeAnim, {
+        toValue: (diagonal / 200) * 1.1,
+        duration: 8000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(colorAnim, {
+        toValue: 1,
+        duration: 8000,
+        useNativeDriver: true,
+      }),
+    ]).start();
   };
 
-  const handlePressOut = () => {
-    // setText('Press me');
-    clearTimeout(timeoutId);
-    // setDisabled(false);
-  };
+const handlePressOut = () => {
+    setPressed(false);
+    clearTimeout(timeoutId1.current);
+    clearTimeout(timeoutId2.current);
+    clearTimeout(timeoutId3.current);
+    clearTimeout(timeoutId4.current);
+    Animated.parallel([
+        Animated.timing(sizeAnim, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+        }),
+        Animated.timing(colorAnim, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: true,
+        }),
+    ]).start();
+    setText('Press me');
+};
 
   return (
     <Pressable
-      style={[styles.button, disabled && styles.disabledButton]}
-      android_ripple={{ color: 'rgba(255, 255, 255, 0.3)' }}
       onPressIn={handlePressIn}
       onPressOut={handlePressOut}
       disabled={disabled}
     >
-      <Text style={styles.text}>{text}</Text>
+      <View
+        style={[
+          styles.buttonContainer,
+          pressed && styles.pressed,
+          disabled && styles.disabledButton,
+        ]}
+      >
+        <Animated.View
+          style={[
+            styles.button,
+            { backgroundColor, transform: [{ scale: sizeAnim }] },
+          ]}
+        />
+        <Text style={styles.text}>{text}</Text>
+      </View>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   button: {
-    backgroundColor: 'blue',
+    position: 'absolute',
+    backgroundColor: 'rgb(195, 117, 0)',
     padding: 10,
-    borderRadius: 5,
     marginTop: 10,
-    width: '50%',
+    width: 200,
+    height: 200,
+    elevation: 15,
+    borderRadius: 250,
   },
   disabledButton: {
     backgroundColor: 'gray',
@@ -56,7 +123,10 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
-    fontSize: 18,
+    fontSize: 24,
+  },
+  pressed: {
+    elevation: 5,
   },
 });
 
